00c04d70da083b02e6d8a275407804c03a834b6c
Ubuntu <ubuntu@ip-172-31-46-37.ec2.internal>
2020-01-02 13:26:28 +0000

    p1&p2 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,51 @@
 
 def sum arr
   # YOUR CODE HERE
+	# arr.sum
+  result = 0
+  arr.each {|x| result = result + x}
+  result 
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+   	# YOUR CODE HERE
+	result = 0
+	if arr.length == 1
+		result = arr[0]
+	elsif arr.length > 1
+		arr.sort!
+		result = arr[-1] + arr[-2]
+	end
+	result
+	# arr.max(2).sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+	arr.combination(2) { |c| retrun true if c.sum == n}
+	false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+	'Hello, ' + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+	Regexp.new('^[qwrtypsdfghjklzxcvbnm]', Regexp::IGNORECASE).match?(s)
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+	/^(0|00|([01]+00))$/.match?(s)
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+	
 end
